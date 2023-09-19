51e934c4da919a07fafa73637b41a1efb46014d7
Abhishek Sachdeva <117009354+AbhishekSachdeva07@users.noreply.github.com>
2023-09-19 15:42:01 +0530

    Create print-in-descending-order.cpp

---
+++ print-in-descending-order.cpp
@@ -0,0 +1,21 @@
+//print number till n
+//in decresing order
+#include <iostream>
+using namespace std;
+void dec(int n)
+{
+    if(n==0)
+    {
+        return;
+    }
+    cout<<n<<" ";
+    dec(n-1);
+}
+int main()
+{
+    int n;
+    cin>>n;
+    dec(n);
+    
+    return 0;
+}
